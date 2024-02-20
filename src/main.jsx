9b0d0e29e0da1fcabd5602d0dff9443c3715e732
JosueXA <josue.xicotencatl.a@gmail.com>
2024-02-20 01:06:12 -0600

    toolkit redux training dataset

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux';
-import App from './App.jsx'
 import './index.css'
 
 import { store } from './store';
-import { PokemonApp } from './PokemonApp.jsx';
+// import App from './App.jsx'
+// import { PokemonApp } from './PokemonApp.jsx';
 import { TodoApp } from './TodoApp.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
